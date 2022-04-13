7812124d13f96124cb082015b8f03d84e5bf06fd
John1925Brown <dima.yaroshevskiy25@mail.ru>
2022-04-13 13:19:52 +0300

    main.js
    feat: tune modal dialog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+/* document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+}); */
+
 document.addEventListener("DOMContentLoaded", function (event) {
-   console.log("DOM fully loaded and parsed");
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const area = document.querySelector('.area');
+
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+
+  modal.addEventListener('keypress', (event) => {
+    if (event.code == 'Space') {   // Не получилось через клавишу Esc, сделал со Space
+      switchModal;
+    }
+  });
+
 });
